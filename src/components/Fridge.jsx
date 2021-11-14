98e4b210324101ce2a676838c9cbdfbffb30b713
Andy Yu <yuandy1031@gmail.com>
2021-11-14 17:59:56 -0500

    chg: add textfield input

--- src/components/Fridge.jsx
+++ src/components/Fridge.jsx
@@ -1,4 +1,5 @@
-import { Grid } from "@mui/material";
+import React, { useState } from 'react';
+import { Grid, Autocomplete, TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
@@ -10,12 +11,30 @@ const useStyles = makeStyles({
 
 function Fridge(props) {
 	const classes = useStyles();
+	const [value, setValue] = useState('');
+	const [ingredients, setIngredients] = useState([]);
 	return (
 		<div>
 			<Grid container className={classes.pageContainer}>
 				<Grid xs/>
-				<Grid xs={8} style={{backgroundColor: 'grey'}}>
-					test
+				<Grid xs={8} style={{backgroundColor: 'lightgrey'}}>
+					<Autocomplete 
+						freeSolo 
+						fullWidth 
+						options={['apple', 'banana', 'pear']} 
+						renderInput={(params) => <TextField {...params} label="add your ingredient" />}
+						value={value}
+						onChange={(event, newInputValue) => {
+							setValue(newInputValue);
+							if ((event.code === 'Enter' && event.type === 'keydown') || (event.type === 'click')) {
+								event.defaultMuiPrevented = true;
+								setIngredients(prevIngredients => [...prevIngredients, newInputValue]);
+							}
+						}}
+					/>
+					{ingredients.map((item, index) => (
+						<div key={index}>{item}</div>
+					))}
 				</Grid>
 				<Grid xs/>
 			</Grid>
