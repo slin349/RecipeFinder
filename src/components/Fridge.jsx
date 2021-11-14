8220bba93043f99e2d7e3baef7a308ca84e14a67
Steven <stevenlin159@gmail.com>
2021-11-14 18:58:10 -0500

    chip styled

--- src/components/Fridge.jsx
+++ src/components/Fridge.jsx
@@ -8,7 +8,18 @@ const useStyles = makeStyles({
 	pageContainer: {
 		height: '100vh',
 		padding: '2rem 0rem',
-	}
+	},
+	chip: {
+		border: '0.15rem solid white',
+		margin: '1rem 2rem 1rem 0rem',
+		borderRadius: '0rem',
+		'& span' : {
+			margin: '0rem 1rem',
+		}
+	},
+	contentContainer: {
+		padding: '2rem 0rem 0rem 2rem',
+	},
 });
 
 function Fridge() {
@@ -22,26 +33,31 @@ function Fridge() {
 	return (
 		<Grid container className={classes.pageContainer}>
 			<Grid item xs/>
-			<Grid item xs={8} style={{backgroundColor: 'lightgrey'}}>
-				<Autocomplete 
-					freeSolo 
-					fullWidth 
-					options={['apple', 'banana', 'pear']} 
-					renderInput={(params) => <TextField {...params} label="add your ingredient" />}
-					value={value}
-					onChange={(event, newInputValue) => {
-						setValue(newInputValue);
-						if ((event.code === 'Enter' && event.type === 'keydown') || (event.type === 'click')) {
-							event.defaultMuiPrevented = true;
-							if (ingredients.indexOf(newInputValue) === -1) {
-								setIngredients(prevIngredients => [...prevIngredients, newInputValue]);
+			<Grid container item xs={8} style={{backgroundColor: 'lightgrey'}}>
+				<Grid item xs={8} className={classes.contentContainer}>
+					<Autocomplete 
+						freeSolo 
+						fullWidth 
+						options={['apple', 'banana', 'pear']} 
+						renderInput={(params) => <TextField {...params} label="add your ingredient" />}
+						value={value}
+						onChange={(event, newInputValue) => {
+							setValue(newInputValue);
+							if ((event.code === 'Enter' && event.type === 'keydown') || (event.type === 'click')) {
+								event.defaultMuiPrevented = true;
+								if (ingredients.indexOf(newInputValue) === -1) {
+									setIngredients(prevIngredients => [...prevIngredients, newInputValue]);
+								}
 							}
-						}
-					}}
-				/>
-				{ingredients.map((item, index) => (
-					<Chip label={item} variant="outlined" onDelete={() => handleDelete(item)} />
-				))}
+						}}
+					/>
+					{ingredients.map((item, index) => (
+						<Chip label={item} variant="outlined" onDelete={() => handleDelete(item)} className={classes.chip} />
+					))}
+				</Grid>
+				<Grid item xs={4}>
+					
+				</Grid>
 				<StickyNote />
 			</Grid>
 			<Grid item xs/>
